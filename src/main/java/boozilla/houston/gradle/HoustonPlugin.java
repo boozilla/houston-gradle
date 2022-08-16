bc1a78cb263b078c36310eaa0ffc6971f1c8f1e1
Kim Chanhwi <kim.ch@hybecorp.com>
2022-08-16 14:07:58 +0900

    Automatically create sync task

--- src/main/java/boozilla/houston/gradle/HoustonPlugin.java
+++ src/main/java/boozilla/houston/gradle/HoustonPlugin.java
@@ -2,6 +2,7 @@ package boozilla.houston.gradle;
 
 import boozilla.houston.gradle.extension.HoustonPluginExtension;
 import boozilla.houston.gradle.extension.HoustonServer;
+import boozilla.houston.gradle.task.SyncAssetProto;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 
@@ -12,5 +13,14 @@ public class HoustonPlugin implements Plugin<Project> {
         final var servers = project.container(HoustonServer.class,
                 name -> project.getObjects().newInstance(HoustonServer.class, name));
         project.getExtensions().create("houston", HoustonPluginExtension.class, servers);
+        project.afterEvaluate(p -> {
+            for(final var server : servers)
+            {
+                final var name = server.getName();
+                final var task = "syncAssetProto" + Character.toUpperCase(name.charAt(0)) + name.substring(1);
+
+                p.getTasks().create(task, SyncAssetProto.class, syncTask -> syncTask.environment = name);
+            }
+        });
     }
 }
